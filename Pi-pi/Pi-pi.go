1f4dbb3f08db74c796fd136c14cb2657f9c02cb0
Loon12 <Focus.aleksandr@gmail.com>
2019-11-07 15:21:25 +0300

    sqrt rand Pi-pi

---
+++ Pi-pi/Pi-pi.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	println(math.Pi)
+	fmt.Println(math.Pi)
+}
---
+++ mathSQRT/mathSQRT.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Printf("Now you have %g problems.", math.Sqrt(7))
+	fmt.Printf("Now you have %e problems.", math.Sqrt(7))
+	fmt.Printf("Now you have %b problems.", math.Sqrt(7))
+}

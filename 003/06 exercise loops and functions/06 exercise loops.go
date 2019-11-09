951ec0b03a725b2ca3f8b57eecae5aea75897c00
Loon12 <Focus.aleksandr@gmail.com>
2019-11-09 22:43:35 +0300

    exercise loops and functions

---
+++ 003/06 exercise loops and functions/06 exercise loops.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const k = 0.0001 // precision
+
+func sqrt(x float64) float64 {
+	var z float64 = 1.0
+	for math.Abs(math.Sqrt(x)-z) >= k {
+		z = z - ((z*z - x) / (2 * z))
+	}
+	return z
+}
+
+func main() {
+	fmt.Println(math.Sqrt(4))
+	fmt.Println(sqrt(4))
+}

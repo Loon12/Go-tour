0815a716881ec5086467a99bd200fd384c997684
Loon12 <Focus.aleksandr@gmail.com>
2019-11-14 22:05:17 +0300

    exercise more best tha early

--- 004/21 exercise-maps/21 exercise maps.go
+++ 004/21 exercise-maps/21 exercise maps.go
@@ -11,13 +11,17 @@ func WordCount(s string) map[string]int {
 	arr := strings.Fields(s)
 
 	for i := 0; i < len(arr); i++ {
-		k := 0
-		for j := 0; j < len(arr); j++ {
-			if arr[i] == arr[j] {
-				k++
+		_, key := m[arr[i]]
+		if key == false {
+			k := 0
+			for j := 0; j < len(arr); j++ {
+				if arr[i] == arr[j] {
+					k++
+				}
 			}
+			m[arr[i]] = k
 		}
-		m[arr[i]] = k
+
 	}
 	return m
 }

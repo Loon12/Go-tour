4a10f6ce57fdd1f8be189ffbccfbdda1ca0c71f6
Loon12 <Focus.aleksandr@gmail.com>
2019-11-14 22:42:52 +0300

    fibonacci

---
+++ 004/24 fibonacci/24 fibonacci.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func fibonacci() func() int {
+	x0 := 0
+	x1 := 1
+	return func() int {
+		x := x0 + x1
+		x0 = x1
+		x1 = x
+		return x0
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}

bc8a395ce3dc716939ece0cec02aedf5f28decc3
Loon12 <Focus.aleksandr@gmail.com>
2019-11-22 17:15:14 +0300

    push

--- 008 maps/02/02.go
+++ 008 maps/02/02.go
@@ -1,2 +1,20 @@
 package main
- 
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	a := make([]int, 1000)
+	for i := range a {
+		a[i] = rand.Intn(100)
+	}
+	//m := make(map[int]int)
+	//printSlice(a)
+}
+func printSlice(x []int) {
+	fmt.Printf("%v   len=%d cap=%d\n", x, len(x), cap(x))
+}

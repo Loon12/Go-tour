890dfb8e87d96fbdaf9b127b71785792859ab200
Loon12 <Focus.aleksandr@gmail.com>
2019-11-22 04:42:25 +0300

    the last

--- 007 slice/01/01.go
+++ 007 slice/01/01.go
@@ -10,9 +10,6 @@ func main() {
 	//fmt.Scanf("%d \n", &t)
 	var s []int
 	for i := 0; i < 5; i++ {
-		//var t1 int
-		//fmt.Printf("Введите элемент массива №%v: ", i)
-		//fmt.Scanf("%f \n" & t1)
 		s = append(s, i)
 	}
 	for i := range s {

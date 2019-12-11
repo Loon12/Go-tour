5fa627cc5ab1bd5c0c76b6c1294abd9198a9f7a8
Loon12 <Focus.aleksandr@gmail.com>
2019-12-11 20:28:32 +0300

    sortABC

--- 007 slise/02-15/02.go
+++ 007 slise/02-15/02.go
@@ -16,23 +16,23 @@ func main() {
 	printSlice(a)
 
 	//2 Добавить в конец слайса число 5
-	Plus5end(&a)
+	add5totheend(&a)
 	printSlice(a)
 
 	//3 Добавить в начало слайса число 5
-	Plus5begin(&a)
+	add5tothebeginning(&a)
 	printSlice(a)
 
 	//4 Взять последнее число слайса, вернуть его пользователю, а из слайса этот элемент удалить
-	delEnd(&a)
+	DelLastElem(&a)
 	printSlice(a)
 
 	//5 Взять первое число слайса, вернуть его пользователю, а из слайса этот элемент удалить
-	delBegin(&a)
+	DelBegElem(&a)
 	printSlice(a)
 
 	//6 Взять i-е число слайса, вернуть его пользователю, а из слайса этот элемент удалить. Число i передает пользователь в функцию
-	del(2, &a)
+	DelElemNumbI(2, &a)
 	printSlice(a)
 
 	//7 Объединить два слайса и вернуть новый со всеми элементами первого и второго
@@ -46,7 +46,7 @@ func main() {
 
 	//8 Из первого слайса удалить все числа, которые есть во втором
 	//a := delel(a, b) если надо из а удалить и зменить значения элементов слайса
-	delel(&b, &a)
+	difference(&b, &a)
 	printSlice(b)
 
 	//9 Сдвинуть все элементы слайса на 1 влево. Нулевой становится последним, первый - нулевым, последний - предпоследним.
@@ -72,20 +72,20 @@ func main() {
 	//printSlice(tmp)
 
 	//14 В слайсе поменять все четные с ближайшими нечетными индексами. 0 и 1, 2 и 3, 4 и 5...
-	neighbouR(&a)
+	neighbouR(a)
 	printSlice(a)
 
 	//15 Упорядочить слайс в порядке: прямом
-	sorT(&a)
+	sorT(a)
 	printSlice(a)
 
 	//обратном
-	sorTBack(&a)
+	sorTBack(a)
 	printSlice(a)
 
 	//лексикографическом
-	s := []string{"asdad", "gafgsaDF", "SDFGSFGSFG", "dfsdfad", "ddfssf", "QWQWQW", "1234", "12335"}
-	sorABC(s)
+	s := []string{"asdad", "asdad", "gafgsaDF", "SDFGSFGSFG", "dfsdfad", "ddfssf", "QWQWQW", "1234", "12335"}
+	sorABC(&s)
 	fmt.Println(s)
 
 }
@@ -96,26 +96,25 @@ func printSlice(x []int) {
 }
 
 //Plus5end функция добавления числа 5 в конец слайса
-func Plus5end(x *[]int) {
+func add5totheend(x *[]int) {
 	*x = append(*x, 5)
 }
 
 //Plus5begin функция добавляет 5 в начало слайса
-func Plus5begin(x *[]int) {
+func add5tothebeginning(x *[]int) {
 	f := []int{5}
-	f = append(f, *x...)
+	f = append(f, (*x)...)
+	printSlice(f)
 	*x = f
 }
 
 //функция возврата и удаления последнего элемента слайса
-func delEnd(a *[]int) {
-	fmt.Println((*a)[len(*a)-1])
+func DelLastElem(a *[]int) {
 	*a = (*a)[:len(*a)-1]
 }
 
 //функция возвората и удаления первого элемента слайса
-func delBegin(a *[]int) {
-	fmt.Println((*a)[0])
+func DelBegElem(a *[]int) {
 	*a = (*a)[1:]
 }
 
@@ -124,12 +123,8 @@ func merge(a *[]int, b *[]int) {
 	*a = append(*a, *b...)
 }
 
-/*функция удаления i-го элемента
-я сделал удаленеие i-го элемента, где a[1] - это первый элемент среза и т.д.
-для удаления i-го элемента, где a[0] - это первый элемент необходимо
-k := y[x:]
-y = y[:x-1]*/
-func del(x int, y *[]int) {
+//функция удаления i-го элемента
+func DelElemNumbI(x int, y *[]int) {
 	*y = append((*y)[:x], (*y)[x+1:]...)
 }
 
@@ -138,7 +133,7 @@ func moveLeft(a *[]int) {
 	*a = append(*a, (*a)[0])
 	*a = (*a)[1:]
 
-	//можно вот так бегать.
+	//можно вот так бегать. тогда сслыку можно использовать
 	/*z := a[0]
 	  for i := range a {
 	      if i == len(a)-1 {
@@ -171,31 +166,75 @@ func moveRightI(a *[]int, d int) {
 }
 
 //функция меняющая местами соседние элементы массива
-func neighbouR(a *[]int) {
-	for i := 0; i < len(*a)-1; i += 2 {
-		(*a)[i], (*a)[i+1] = (*a)[i+1], (*a)[i]
+func neighbouR(a []int) {
+	for i := 0; i < len(a)-1; i += 2 {
+		a[i], a[i+1] = a[i+1], a[i]
 	}
 }
 
-//пузырьковая прямая сортировка
-func sorT(a *[]int) {
-	sort.Ints(*a)
+//прямая сортировка
+func sorT(a []int) {
+	sort.Ints(a)
 }
 
-//пузырьковая обратная сортировка
-func sorTBack(a *[]int) {
-	sort.Ints(*a)
-	for i := 0; i < len(*a)/2; i++ {
-		(*a)[i], (*a)[len(*a)-i-1] = (*a)[len(*a)-i-1], (*a)[i]
-	}
+//обратная сортировка
+func sorTBack(a []int) {
+	sort.Sort(sort.Reverse(sort.IntSlice(a)))
 }
 
 //сортировка в лексикографическом порядке
-func sorABC(a []string) {
-	sort.Strings(a)
+func sorABC(a *[]string) {
+	r := make([][]rune, len(*a))
+	for i := range *a {
+		r[i] = []rune((*a)[i])
+	}
+	for i := 0; i < len(*a); i++ {
+		for j := i + 1; j < len(*a); j++ {
+			if comparison(r[i], r[j]) == false {
+				r[i], r[j] = r[j], r[i]
+				(*a)[i], (*a)[j] = (*a)[j], (*a)[i]
+			}
+		}
+	}
 }
 
-func delel(a *[]int, b *[]int) {
+//comparison - функция сравнения двух рун. если первая меньше либо равна второй (в лексикографическом смысле), то возвращается истина, в противном случае ложь
+func comparison(a []rune, b []rune) bool {
+	var g bool
+	for i := 0; i < min(a, b); i++ {
+		if a[i] < b[i] {
+			g = true
+			break
+		}
+		if a[i] > b[i] {
+			g = false
+			break
+		}
+		if i == min(a, b)-1 {
+			if len(a) <= len(b) {
+				g = true
+				break
+			}
+			if len(a) > len(b) {
+				g = false
+				break
+			}
+		}
+	}
+	return g
+}
+
+//функция, которая возвражает длину меньшей руны
+func min(a []rune, b []rune) int {
+	if len(a) <= len(b) {
+		return len(a)
+	} else {
+		return len(b)
+	}
+}
+
+//функция разности двух множеств
+func difference(a *[]int, b *[]int) {
 	for i := 0; i < len(*a); i++ {
 		for j := 0; j < len(*b); j++ {
 			if (*a)[i] == (*b)[j] {
--- 008 maps/00/00.go
+++ 008 maps/00/00.go
@@ -1,49 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
+import "fmt"
+
+func ten(s []int) {
+	for i := range s {
+		s[i] = 10
+	}
+}
+
+func addone(s []int) []int {
+	s = append(s, 1)
+	return s
+}
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-
-	a := make([]int, 10)
-	for i := range a {
-		a[i] = rand.Intn(10)
-	}
-
-	b := make([]int, 10)
-	for i := range b {
-		b[i] = rand.Intn(10)
-	}
-
+	a := make([]int, 9)
+	ten(a)
+	a = addone(a)
 	fmt.Println(a)
-	fmt.Println(b)
-
-	delel(&a, &b)
-
-	fmt.Println(a)
-
-}
-
-func delel(a *[]int, b *[]int) {
-	for i := 0; i < len(*a); i++ {
-		for j := 0; j < len(*b); j++ {
-			if (*a)[i] == (*b)[j] {
-				if i == len(*a)-1 {
-					*a = (*a)[:i]
-					break
-				} else {
-					*a = append((*a)[:i], (*a)[i+1:]...)
-					j = -1
-				}
-			}
-		}
-	}
-}
-
-func printSlice(x []int) {
-	fmt.Printf("%v   len=%d cap=%d\n", x, len(x), cap(x))
 }
